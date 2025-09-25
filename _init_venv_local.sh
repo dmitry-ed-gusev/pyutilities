2ebe7a77cd9648c316ab3d2ceeb21b9dd691f379
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2025-09-25 23:55:09 +0300

    Minor script update.

--- _init_venv_local.sh
+++ _init_venv_local.sh
@@ -5,7 +5,7 @@
 #   Python virtual environment initialization script for the project [pyutilities].
 #
 #   Created:  Dmitrii Gusev, 21.07.2025
-#   Modified:
+#   Modified: Dmitrii Gusev, 25.09.2025
 #
 # ###################################################################################################
 
@@ -19,9 +19,10 @@ export _CURRENT_DATE
 _CURRENT_TIME=$(date +"%H:%M:%S") || { printf "\nError while calculating system time!\n"; sleep 3; exit 1; }
 export _CURRENT_TIME
 
-# # -- some useful script defaults
-# export _VERBOSE="--verbose"
-# export _VIRTUAL_ENV_NAME=".venv--3.10"
+# -- some useful script defaults
+export _VERBOSE="--verbose"
+export _MSG_END_OF_STEP="========== done."
+export _VIRTUAL_ENV_NAME=".venv--3.10"
 # export _PIP_TRUSTED_HOST="--trusted-host pypi.org"
 # export _REQUIREMENTS_FILE='requirements.txt' # prod file
 # export _REQUIREMENTS_DEV_FILE='requirements-dev.txt' # dev file
@@ -32,43 +33,42 @@ export _CURRENT_TIME
 clear
 printf "=== %s %s - Python Virtual Env initializing... ===\n\n" "${_CURRENT_DATE}" "${_CURRENT_TIME}"
 
-# # -- check the machine and determine the python/pip versions
-# printf "\n= INFO: checking the machine architecture and python/pip versions.\n"
-# unameOut="$(uname -s)" # get machine name (short)
-# # - based on the machine type - setup aliases (env variables)
-# case "${unameOut}" in
-#     Linux*)     export _MACHINE_TYPE=Linux;  export _CMD_PYTHON=python3; export _CMD_PIP=pip3;;
-#     Darwin*)    export _MACHINE_TYPE=Mac;    export _CMD_PYTHON=python3; export _CMD_PIP=pip3;;
-#     CYGWIN*)    export _MACHINE_TYPE=Cygwin; export _CMD_PYTHON=python;  export _CMD_PIP=pip;; # win emu
-#     MINGW*)     export _MACHINE_TYPE=MinGW;  export _CMD_PYTHON=python;  export _CMD_PIP=pip;; # win emu
-#     *)          printf "Unknown machine: [%s]!" "${unameOut}"; exit 1;;
-# esac
-# # - debug output I - machine/python/pip
-# printf "\n= INFO: Machine type: [%s], using python: [%s], using pip: [%s].\n" \
-#     "${_MACHINE_TYPE}" "${_CMD_PYTHON}" "${_CMD_PIP}"
-# # - debug output II - python/pip versions
-# printf "\n= INFO: Using python 3/pip 3 versions:\n"
-# printf "\t"; "${_CMD_PYTHON}" --version || { printf "\n%s\n" "${_MSG_NO_SYS_PYTHON}"; sleep 5; exit 1; }
-# printf "\t"; "${_CMD_PIP}" --version || { printf "\n%s\n" "${_MSG_NO_SYS_PIP}"; sleep 5; exit 1; }
+# -- Step I. Check the machine and determine the python/pip versions, print them.
+printf "\n= INFO: Step I. Checking the machine architecture and python/pip versions.\n"
+unameOut="$(uname -s)" # get machine name (short)
+# - based on the machine type - setup aliases (env variables)
+case "${unameOut}" in
+    Linux*)     export _MACHINE_TYPE=Linux;  export _CMD_PYTHON=python3; export _CMD_PIP=pip3;;
+    Darwin*)    export _MACHINE_TYPE=Mac;    export _CMD_PYTHON=python3; export _CMD_PIP=pip3;;
+    CYGWIN*)    export _MACHINE_TYPE=Cygwin; export _CMD_PYTHON=python;  export _CMD_PIP=pip;; # win emu
+    MINGW*)     export _MACHINE_TYPE=MinGW;  export _CMD_PYTHON=python;  export _CMD_PIP=pip;; # win emu
+    *)          printf "Unknown machine: [%s]!" "${unameOut}"; exit 1;;
+esac
+# - debug output I - machine type/python cmd/pip cmd
+printf "\n= INFO: Machine type: [%s], using python: [%s], using pip: [%s].\n" \
+    "${_MACHINE_TYPE}" "${_CMD_PYTHON}" "${_CMD_PIP}"
+# - debug output II - python version/pip version
+printf "\n= INFO: Using python 3/pip 3 versions:\n"
+printf "\t"; "${_CMD_PYTHON}" --version || { printf "\n%s\n" "${_MSG_NO_SYS_PYTHON}"; sleep 5; exit 1; }
+printf "\t"; "${_CMD_PIP}" --version || { printf "\n%s\n" "${_MSG_NO_SYS_PIP}"; sleep 5; exit 1; }
+# - debug output III - show python paths
+printf "\n= INFO: \n"; $_CMD_PYTHON -m site; printf "\n"
+# - show python packages dirs (global+user) <- python code
+$_CMD_PYTHON - << END
+import site
+global_pkg = site.getsitepackages()
+users_pkg = site.getusersitepackages()
+print(f"\n= INFO:\nglobal packages path: [{global_pkg}].\nuser packages path: [{users_pkg}].")
+END
+printf "\n%s\n" "${_MSG_END_OF_STEP}"
 
-# # -- show python paths
-# printf "\n= INFO: "; $_CMD_PYTHON -m site; printf "\n"
-# # - show python packages dirs (global+user) <- python code
-# $_CMD_PYTHON - << END
-# import site
-# global_pkg = site.getsitepackages()
-# users_pkg = site.getusersitepackages()
-# print(f"global packages path: [{global_pkg}].\nuser packages path: [{users_pkg}].")
-# END
-# printf "\n========== done.\n"
-
-# # -- upgrade pip + setuptools to the latest versions
-# printf "\n= INFO: Upgrading PIP + SETUPTOOLS in the global python environment.\n"
-# # shellcheck disable=SC2086
-# ${_CMD_PYTHON} -m pip ${_VERBOSE} --no-cache-dir install --upgrade pip
-# # shellcheck disable=SC2086
-# ${_CMD_PIP} ${_VERBOSE} --no-cache-dir install --upgrade setuptools
-# printf "\n========== done.\n"
+# -- Step II. Upgrade pip + setuptools to the latest versions (on the current python).
+printf "\n= INFO: Step II. Upgrading PIP + SETUPTOOLS in the global python environment.\n"
+# shellcheck disable=SC2086
+${_CMD_PYTHON} -m pip ${_VERBOSE} --no-cache-dir install --upgrade pip
+# shellcheck disable=SC2086
+${_CMD_PIP} ${_VERBOSE} --no-cache-dir install --upgrade setuptools
+printf "\n%s\n" "${_MSG_END_OF_STEP}"
 
 # # -- remove existing virtual environment
 # printf "\n= INFO: removing existing virtual environment at [%s].\n" "${_VIRTUAL_ENV_NAME}"
